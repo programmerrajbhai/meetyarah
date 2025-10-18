27a11260bb990fe6b081f07148f29db382b900ad
king <mdrajahmad686@gmail.com>
2025-10-18 10:46:27 -0700

    tab bar implement

--- lib/ui/home/screens/baseScreens.dart
+++ lib/ui/home/screens/baseScreens.dart
@@ -13,48 +13,81 @@ class Basescreens extends StatefulWidget {
 class _BasescreensState extends State<Basescreens> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(15),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Text(
-                  "SImple World",
-                  style: TextStyle(
-                    color: ColorPath.deepBlue,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30,
-                  ),
-                ),
-                Spacer(),
-                Row(
-                  children: [
-                    CircleAvatar(
-                      backgroundColor: ColorPath.softGray,
-                      child: IconButton(onPressed: (){},
-                          icon: Icon(Icons.search,
-                            color: Colors.black,
-                          size: 24,
-                          )),
+    return DefaultTabController(
+      length: 6,
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.all(15),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Text(
+                    "SImple World",
+                    style: TextStyle(
+                      color: ColorPath.deepBlue,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
                     ),
-                    CircleAvatar(
-                      backgroundColor: ColorPath.softGray,
-                      child: IconButton(onPressed: (){},
-                          icon: Icon(Icons.sms,
+                  ),
+                  Spacer(),
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: ColorPath.softGray,
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.search,
                             color: Colors.black,
                             size: 24,
-                          )),
-                    ),
-
+                          ),
+                        ),
+                      ),
+                      CircleAvatar(
+                        backgroundColor: ColorPath.softGray,
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.sms, color: Colors.black, size: 24),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Container(
+                child: TabBar(
+                  labelColor: Colors.black,
+                  // সিলেক্টেড ট্যাবের লেখার রঙ
+                  unselectedLabelColor: Colors.black,
+                  // অন্যান্য ট্যাবের লেখার রঙ
+                  indicatorColor: ColorPath.softGray,
+                  // ইন্ডিকেটরের রঙ
+                  indicatorWeight: 3.0,
+                  tabs: [
+                    Tab(icon: Icon(Icons.home_outlined)),
+                    Tab(icon: Icon(Icons.explore_outlined)),
+                    Tab(icon: Icon(Icons.add_box_outlined)),
+                    Tab(icon: Icon(Icons.person_outline)),
+                    Tab(icon: Icon(Icons.insights)),
+                    Tab(icon: Icon(Icons.apps)),
                   ],
-                )
-              ],
-            ),
-
-
-          ],
+                ),
+              ),
+              Expanded(
+                child: TabBarView(
+                  children: [
+                    Text("SCreens 1"),
+                    Text("SCreens 2"),
+                    Text("SCreens 3"),
+                    Text("SCreens 4"),
+                    Text("SCreens 5"),
+                    Text("SCreens 6"),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
